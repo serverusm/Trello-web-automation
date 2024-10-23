b84bb52c425383619d9230b944665228b9eb6e0e
ServerusM <sergiomamani2014@gmail.com>
2024-10-22 21:41:59 -0400

    Added a WebDriverFactoryTest class

---
+++ trello-web-automation/core-ui-automation/src/test/java/com/qaacademy/module5/automation/core/ui/webdivers/WebDriverFactoryTest.java
@@ -0,0 +1,29 @@
+package com.qaacademy.module5.automation.core.ui.webdivers;
+
+import com.qaacademy.module5.automation.core.ui.webdriver.WebDriverFactory;
+import com.qaacademy.module5.automation.core.ui.webdriver.WebDriverTypes;
+import org. junit. jupiter.api.AfterEach;
+import org. junit. jupiter.api.DisplayName;
+import org. junit.jupiter.api.Tag;
+import org. junit.jupiter.params. ParameterizedTest;
+import org. junit. jupiter.params.provider. EnumSource;
+import org.openqa.selenium.WebDriver;
+
+import static org. junit.jupiter.api.Assertions.assertNotNull;
+
+@DisplayName(("WebDriver Factory Test"))
+class WebDriverFactoryTest {
+  private WebDriver webDriver;
+
+  @AfterEach
+  void teardown(){ webDriver.quit();}
+
+  @ParameterizedTest
+  @EnumSource
+  @DisplayName("Verifies if getDriver call is not null")
+  @Tag("UniTest")
+  void verifiesIfGetDriverCallIsNotNull(final WebDriverTypes webDriverTypes){
+    webDriver = WebDriverFactory.getWebDriverManager(webDriverTypes).getWebDriver();
+    assertNotNull(webDriver);
+  }
+}
