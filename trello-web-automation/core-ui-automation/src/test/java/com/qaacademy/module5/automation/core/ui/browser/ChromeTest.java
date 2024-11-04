21ec1baad60ad2ff990f040a4292ce00908e1146
ServerusM <sergiomamani2014@gmail.com>
2024-11-03 23:21:16 -0400

    Changes in class for ChromeTest Running

--- trello-web-automation/core-ui-automation/src/test/java/com/qaacademy/module5/automation/core/ui/browser/ChromeTest.java
+++ trello-web-automation/core-ui-automation/src/test/java/com/qaacademy/module5/automation/core/ui/browser/ChromeTest.java
@@ -1,38 +1,44 @@
 package com.qaacademy.module5.automation.core.ui.browser;
 
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Chrome Browser Test")
 class ChromeTest {
   public static final String GOOGLE_URL = "https://www.google.com/";
   public static final String GOOGLE = "Google";
-  WebDriver driver;
-
-  @BeforeAll
-  static void setupAll() {
-    WebDriverManager.chromedriver().setup();
-  }
+  private Chrome chrome;
+  private WebDriverManager webDriver;
 
   @BeforeEach
   void setup() {
-    driver = new ChromeDriver();
+    WebDriverManager.chromedriver().setup();
+    chrome = new Chrome();// Start chrome
   }
 
   @AfterEach
   void teardown() {
-    driver.quit();
+    webDriver.quit();
+  }
+
+  @Test
+  @DisplayName("Verifies if getDriver call is not null")
+  void verifesIfGetDriverCAllIsNotNull() {
+    webDriver = chrome.getDriver();
+    assertNotNull(webDriver);
   }
 
   @Test
   @DisplayName("Verifies if browser is opening")
-  void test() {
-    driver.navigate().to(GOOGLE_URL);
-    assertTrue(driver.getTitle().contains(GOOGLE));
+  void verifiesIfBrowserIsOpening() {
+    webDriver = chrome.getDriver();
+    webDriver.getWebDriver().navigate().to(GOOGLE_URL);
+    assertTrue(webDriver.getWebDriver().getTitle().contains(GOOGLE));
   }
 }
